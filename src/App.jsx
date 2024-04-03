be824fe5fe5cf2ac766d7f640695d33c96e769e1
techbossng <chukwumapaulinus@gmail.com>
2024-04-03 13:04:11 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ const App = () => {
       <Hireme />
       <Contact />
       <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
+        <h6 className="mb-3">AIDEMAX CONSULTANT</h6>
+        <p>Aidemax © All CopyRights Reserved 2024</p>
       </footer>
     </div>
   );
